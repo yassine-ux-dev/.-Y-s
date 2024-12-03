6a673daffc51e04f1f8894abecd4f1195143d6d3
yassine0221 <charradayassine3@gmail.com>
2024-12-03 23:32:24 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.pdf$/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/media/[name].[hash][ext]", // Customize file output location
+      },
+    });
+    return config;
+  },
+  images: { domains: ["github.com", "fonts.gstatic.com"] },
+};
+
+module.exports = nextConfig;
